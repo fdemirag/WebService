59e5aab7663f68157c9177efb7dd37d4159dec31
fdemirag <f2000fatih@hotmail.com>
2023-12-24 03:20:48 +0300

    migration olusturuldu

---
+++ HotelProject.DataAccess/Concretes/Context.cs
@@ -0,0 +1,23 @@
+﻿using HotelProject.EntityLayer.Concretes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Concretes
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Initial Catalog=ApiDb;Integrated Security=True;");
+        }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
