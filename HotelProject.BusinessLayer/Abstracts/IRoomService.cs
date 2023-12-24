11246bb9c4f809e407eb41a188a0460d9ff3c0cc
fdemirag <f2000fatih@hotmail.com>
2023-12-24 04:14:53 +0300

    Business abstract ve concrete classları oluşturuldu

---
+++ HotelProject.BusinessLayer/Abstracts/IRoomService.cs
@@ -0,0 +1,13 @@
+﻿using HotelProject.EntityLayer.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Abstracts
+{
+    public interface IRoomService :IGenericService<Room>
+    {
+    }
+}
